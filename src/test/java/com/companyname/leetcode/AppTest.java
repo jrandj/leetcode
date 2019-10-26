56c589701f4d970adf2d88b9673df3195ec8edc4
jrandj <james.randjelovic@gmail.com>
2019-10-26 17:11:26 +1100

    Add solution for "Add Two Numbers"

--- src/test/java/com/companyname/leetcode/AppTest.java
+++ src/test/java/com/companyname/leetcode/AppTest.java
@@ -1,10 +1,13 @@
 package com.companyname.leetcode;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Arrays;
-
+import java.util.LinkedList;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
 
 /**
  * Unit test for simple App.
@@ -12,6 +15,7 @@ import junit.framework.TestSuite;
 public class AppTest extends TestCase
 {
 	
+  
 	public void testtwoSumExpectedResult1() {
 		int[] result;
 		int[] nums = {2, 7, 11, 15};
@@ -22,6 +26,7 @@ public class AppTest extends TestCase
 		assertEquals(Arrays.toString(testResult), Arrays.toString(result));
 	}
 	
+	
 	public void testtwoSumExpectedResult2() {
 		int[] result;
 		int[] nums = {3, 2, 3};
@@ -31,6 +36,7 @@ public class AppTest extends TestCase
 		result = com.companyname.leetcode.App.twoSum(nums, target);
 		assertEquals(Arrays.toString(testResult), Arrays.toString(result));
 	}
+	
 	
 	public void testtwoSumExpectedResult3() {
 		int[] result;
@@ -43,6 +49,50 @@ public class AppTest extends TestCase
 	}
 	
 	
+    public void testaddTwoNumbers1() {
+      ListNode l1n1 = new ListNode(2);
+      ListNode l1n2 = new ListNode(4);
+      ListNode l1n3 = new ListNode(3);
+      l1n1.next = l1n2;
+      l1n2.next = l1n3;
+
+      ListNode l2n1 = new ListNode(5);
+      ListNode l2n2 = new ListNode(6);
+      ListNode l2n3 = new ListNode(4);
+      l2n1.next = l2n2;
+      l2n2.next = l2n3;
+      
+      List<Integer> testResult = new ArrayList<Integer>();
+      List<Integer> actualResult = new ArrayList<Integer>(); 
+      testResult.add(7);
+      testResult.add(0);
+      testResult.add(8);
+       
+      ListNode result = com.companyname.leetcode.App.addTwoNumbers(l1n1, l2n1);
+      while (result != null) {
+        actualResult.add(result.val);
+        result = result.next;
+      }
+      assertEquals(testResult, actualResult);
+    }
+    
+    public void testaddTwoNumbers2() {
+      ListNode l1n1 = new ListNode(5);
+      ListNode l2n1 = new ListNode(5);
+      
+      List<Integer> testResult = new ArrayList<Integer>();
+      List<Integer> actualResult = new ArrayList<Integer>(); 
+      testResult.add(0);
+      testResult.add(1);
+
+      ListNode result = com.companyname.leetcode.App.addTwoNumbers(l1n1, l2n1);
+      while (result != null) {
+        actualResult.add(result.val);
+        result = result.next;
+      }
+      assertEquals(testResult, actualResult);
+    }
+	   
     /**
      * Create the test case
      *
@@ -62,6 +112,7 @@ public class AppTest extends TestCase
         return new TestSuite( AppTest.class );
     }
 
+    
     /**
      * Rigourous Test :-)
      */

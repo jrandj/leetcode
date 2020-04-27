0c7f7566283f8b3f3704906d1a035521c5c2083a
jrandj <james.randjelovic@gmail.com>
2020-04-27 13:21:32 +1000

    Add solution for 16. 3Sum Closest

--- src/main/java/leetcode/ThreeSumClosest.java
+++ src/main/java/leetcode/ThreeSumClosest.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * Given an array nums of n integers and an integer target, find three integers
  * in nums such that the sum is closest to target. Return the sum of the three
@@ -15,13 +17,37 @@ class ThreeSumClosest {
 	/**
 	 * Return the sum closest to the target.
 	 *
-	 * @param nums the array of ints
+	 * @param nums   the array of ints
 	 * @param target the target sum
 	 * @return the sum of the closest 3 ints
 	 */
-
 	public static int threeSumClosest(int[] nums, int target) {
-		return target;
+		int currentDifference;
+		int previousDifference;
+		previousDifference = Integer.MAX_VALUE;
 
+		int returnValue = 0;
+		Arrays.sort(nums);
+		for (int i = 0; i < nums.length - 2; i++) {
+			// low and high are the bounds for the subsequent 2 numbers
+			int low = i + 1;
+			int high = nums.length - 1;
+			while (low < high) {
+				currentDifference = Math.abs(nums[i] + nums[low] + nums[high] - target); // minimise this value
+				if (currentDifference < previousDifference) { // getting closer to target
+					previousDifference = currentDifference;
+					returnValue = nums[i] + nums[low] + nums[high];
+				}
+
+				// need smaller number, exclude the largest number
+				if (nums[i] + nums[low] + nums[high] > target) {
+					high--;
+				// need larger number, exclude the smallest number
+				} else {
+					low++;
+				}
+			}
+		}
+		return returnValue;
 	}
 }
--- src/test/java/leetcode/ThreeSumClosestTest.java
+++ src/test/java/leetcode/ThreeSumClosestTest.java
@@ -10,4 +10,28 @@ public class ThreeSumClosestTest extends TestCase {
 		int testResult = 2;
 		assertEquals(testResult, result);
 	}
+
+	public void testthreeSumClosest2() {
+		int[] nums = { 1, 1, 1, 0 };
+		int target = -100;
+		int result = leetcode.ThreeSumClosest.threeSumClosest(nums, target);
+		int testResult = 2;
+		assertEquals(testResult, result);
+	}
+
+	public void testthreeSumClosest3() {
+		int[] nums = { 0, 2, 1, -3 };
+		int target = 1;
+		int result = leetcode.ThreeSumClosest.threeSumClosest(nums, target);
+		int testResult = 0;
+		assertEquals(testResult, result);
+	}
+
+	public void testthreeSumClosest4() {
+		int[] nums = { 1, 1, -1, -1, 3 };
+		int target = -1;
+		int result = leetcode.ThreeSumClosest.threeSumClosest(nums, target);
+		int testResult = -1;
+		assertEquals(testResult, result);
+	}
 }

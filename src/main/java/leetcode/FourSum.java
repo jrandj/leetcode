d3f9b9029b7e91ab974dc0c60476057cbcccfe66
jrandj <james.randjelovic@gmail.com>
2020-05-09 18:15:52 +1000

    Initial commit

---
+++ src/main/java/leetcode/FourSum.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+import java.util.List;
+
+/**
+ * Given an array nums of n integers and an integer target, are there elements
+ * a, b, c, and d in nums such that a + b + c + d = target? Find all unique
+ * quadruplets in the array which gives the sum of target.
+ * 
+ * Note:
+ * 
+ * The solution set must not contain duplicate quadruplets.
+ */
+class FourSum {
+	/**
+	 * Return valid 4sum solutions.
+	 *
+	 * @param s the input array
+	 * @param target the target
+	 * @return the list of valid 4sum solutions
+	 */
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+    	
+    	// sort nums
+    	// left pointer and right pointer
+    	// left pointer at zero right pointer at len nums
+    	// 
+    	// 1, 0, -1, 0, -2, 2
+    	// -2, -1, 0, 0, 1, 2
+    	// for each number, look for 3 subsequent numbers that give a total sum of 0
+    	
+    	
+    	
+    	
+    	
+    	
+    	return null;
+    }
+}
---
+++ src/test/java/leetcode/FourSumTest.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+import java.util.List;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class FourSumTest extends TestCase {
+	public void testGenerateParenthesesTest1() {
+		int[] n = {1, 0, -1, 0, -2, 2};
+		int target = 0;
+		List<List<Integer>> result = leetcode.FourSum.fourSum(n, target);
+		List<List<Integer>> testResult = List.of(List.of(-1,  0, 0, 1), List.of(-2, -1, 1, 2), List.of(-2,  0, 0, 2));
+		Assert.assertEquals(testResult, result);
+	}
+
+}

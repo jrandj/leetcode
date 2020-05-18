ca95f8834c40b0438b94e170b7d8b112d6269208
jrandj <james.randjelovic@gmail.com>
2020-05-18 20:04:51 +1000

    Initial commit

---
+++ src/main/java/leetcode/SortList.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import leetcode.AddTwoNumbers.ListNode;
+
+/**
+ * Sort a linked list in O(n log n) time using constant space complexity.
+ */
+class SortList {
+	/**
+	 * Sort a linked list.
+	 *
+	 * @param head the head of the unsorted linked list
+	 * @return head the head of sorted linked list
+	 */
+	public ListNode sortList(ListNode head) {
+
+		return head;
+	}
+}
---
+++ src/test/java/leetcode/SortListTest.java
@@ -0,0 +1,68 @@
+package leetcode;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import leetcode.AddTwoNumbers.ListNode;
+
+public class SortListTest extends TestCase {
+	public void testSortList1() {
+		ListNode l1n1 = new ListNode(4);
+		ListNode l1n2 = new ListNode(2);
+		ListNode l1n3 = new ListNode(1);
+		ListNode l1n4 = new ListNode(3);
+		l1n1.next = l1n2;
+		l1n2.next = l1n3;
+		l1n3.next = l1n4;
+
+		ListNode l2n1 = new ListNode(1);
+		ListNode l2n2 = new ListNode(2);
+		ListNode l2n3 = new ListNode(3);
+		ListNode l2n4 = new ListNode(4);
+		l2n1.next = l2n2;
+		l2n2.next = l2n3;
+		l2n3.next = l2n4;
+
+		SortList instance = new SortList();
+		ListNode actualResult = instance.sortList(l1n1);
+		Assert.assertTrue(CompareLists(actualResult, l2n1));
+	}
+
+	public void testSortList2() {
+		ListNode l1n1 = new ListNode(-1);
+		ListNode l1n2 = new ListNode(5);
+		ListNode l1n3 = new ListNode(3);
+		ListNode l1n4 = new ListNode(4);
+		ListNode l1n5 = new ListNode(0);
+		l1n1.next = l1n2;
+		l1n2.next = l1n3;
+		l1n3.next = l1n4;
+		l1n4.next = l1n5;
+
+		ListNode l2n1 = new ListNode(-1);
+		ListNode l2n2 = new ListNode(0);
+		ListNode l2n3 = new ListNode(3);
+		ListNode l2n4 = new ListNode(4);
+		ListNode l2n5 = new ListNode(5);
+		l2n1.next = l2n2;
+		l2n2.next = l2n3;
+		l2n3.next = l2n4;
+		l2n4.next = l2n5;
+
+		SortList instance = new SortList();
+		ListNode actualResult = instance.sortList(l1n1);
+		Assert.assertTrue(CompareLists(actualResult, l2n1));
+	}
+
+	boolean CompareLists(ListNode headA, ListNode headB) {
+		if ((headA == null) ^ (headB == null))
+			return false;
+
+		if ((headA == null) && (headB == null))
+			return true;
+
+		if (headA.val != headB.val)
+			return false;
+		return CompareLists(headA.next, headB.next);
+	}
+
+}

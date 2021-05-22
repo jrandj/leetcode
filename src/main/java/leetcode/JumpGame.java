5f564107e95e0d91a927454000b06f3f413bf05a
jrandj <james.randjelovic@gmail.com>
2021-05-22 18:12:43 +1000

    Update solution for 55. Jump Game

--- src/main/java/leetcode/JumpGame.java
+++ src/main/java/leetcode/JumpGame.java
@@ -14,15 +14,24 @@ public class JumpGame {
     /**
      * Determine if you can reach the last index.
      *
-     * The time complexity is O(2^N) as you make 2 decisions N times.
+     * The time complexity is O(2^N) as you make 2 decisions N times. Adding the
+     * visited array reduces the time complexity to O(N^2) as for each element
+     * you are iterating up to the farthest jump.
      *
-     * The space complexity is O(N) for the recursive stack frames.
+     * The space complexity is O(N) for the recursive stack frames. Adding the
+     * visited array adds O(N) space for the array, but O(2N) is still O(N).
      *
      * @param nums The input array.
      * @return True if you can and false if you cannot.
      */
     public boolean canJump(final int[] nums) {
-        return canJumpFromPosition(nums, 0);
+
+        if (nums.length < 1 || nums.length > 10e5) {
+            return false;
+        }
+
+        boolean[] visited = new boolean[nums.length];
+        return canJumpFromPosition(nums, 0, visited);
     }
 
     /**
@@ -30,25 +39,61 @@ public class JumpGame {
      *
      * @param nums     The input array.
      * @param position The current position.
+     * @param visited  The array tracking the visited indices.
      * @return True if you can to the next position and false if you cannot.
      */
-    private boolean canJumpFromPosition(final int[] nums, final int position) {
+    private boolean canJumpFromPosition(final int[] nums, final int position,
+            final boolean[] visited) {
+
+        if (nums[position] > 10e5) {
+            return false;
+        }
 
         // we have reached the end
         if (position == nums.length - 1) {
             return true;
         }
 
+        visited[position] = true;
         int furthestJump = nums[position];
 
         // do all of the jumps
         for (var i = 1; i <= furthestJump; i++) {
-            if (canJumpFromPosition(nums, position + i)) {
+
+            if (!visited[position + i]
+                    && canJumpFromPosition(nums, position + i, visited)) {
                 return true;
             }
         }
 
         return false;
+    }
+
+    /**
+     * Determine if you can reach the last index.
+     *
+     * The time complexity is O(N) as the array is traversed once.
+     *
+     * The space complexity is constant.
+     *
+     * @param nums The input array.
+     * @return True if you can and false if you cannot.
+     */
+    public boolean canJumpGreedy(final int[] nums) {
+
+        if (nums.length < 1 || nums.length > 10e5) {
+            return false;
+        }
+
+        int lastGoodIndex = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            // this means we can jump from nums[i] to the next good index
+            if (nums[i] + i >= lastGoodIndex) {
+                lastGoodIndex = i;
+            }
+        }
+
+        return lastGoodIndex == 0;
     }
 
 }
--- src/test/java/leetcode/JumpGameTest.java
+++ src/test/java/leetcode/JumpGameTest.java
@@ -26,4 +26,26 @@ public class JumpGameTest extends TestCase {
         assertEquals(output, testResult);
     }
 
+    /**
+     * The third test for Jump Game.
+     */
+    public void testJumpGameTest3() {
+        int[] input = new int[] {2, 3, 1, 1, 4};
+        boolean testResult = true;
+        JumpGame jumpGame = new JumpGame();
+        boolean output = jumpGame.canJumpGreedy(input);
+        assertEquals(output, testResult);
+    }
+
+    /**
+     * The fourth test for Jump Game.
+     */
+    public void testJumpGameTest4() {
+        int[] input = new int[] {3, 2, 1, 0, 4};
+        boolean testResult = false;
+        JumpGame jumpGame = new JumpGame();
+        boolean output = jumpGame.canJumpGreedy(input);
+        assertEquals(output, testResult);
+    }
+
 }

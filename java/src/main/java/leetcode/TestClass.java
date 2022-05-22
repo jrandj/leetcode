1bfe64278316c2678ef5fa5b0c553258fc0a97ae
jrandj <james.randjelovic@gmail.com>
2022-05-22 14:54:08 +1000

    Refactor and update README.md

---
+++ java/src/main/java/leetcode/TestClass.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * TBC.
+ *
+ */
+public class TestClass {
+
+//    public static void main(final String[] args) {
+////        int expr1 = 3 + 5 * 9 - 7;
+////        int expr2 = 3 + (5 * 9) - 7;
+////        int expr3 = 3 + 5 * (9 - 7);
+////        int expr4 = (3 + 5) * 9 - 7;
+//    }
+}
---
+++ java/src/main/java/leetcode/TestClassTest.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+public class TestClassTest {
+
+//    public static void main(String args[]) {
+//        TestClass testClass = new TestClass();
+//        //testClass.main(null);
+//    }
+
+}
